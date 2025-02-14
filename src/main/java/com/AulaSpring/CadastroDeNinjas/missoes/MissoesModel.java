ee719ae76348b6c988707071f11ce462ef877891
JReis <jedor.reis@gmail.com>
2025-02-14 18:41:19 -0300

    configurações para o DB

--- src/main/java/com/AulaSpring/CadastroDeNinjas/missoes/MissoesModel.java
+++ src/main/java/com/AulaSpring/CadastroDeNinjas/missoes/MissoesModel.java
@@ -2,10 +2,17 @@ package com.AulaSpring.CadastroDeNinjas.missoes;
 
 import com.AulaSpring.CadastroDeNinjas.ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
-@Entity // Transforma classe em entidade do DB
-@Table(name = "tb_missoes") // Criar tabela
+@Entity
+@Table(name = "tb_missoes")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class MissoesModel {
 
     @Id
