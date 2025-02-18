bc5e2b514319d8af45f75819d2d72145e322cc0a
JReis <jedor.reis@gmail.com>
2025-02-18 15:17:01 -0300

    criação das rotas de ninja com mock

--- src/main/java/com/AulaSpring/CadastroDeNinjas/ninjas/NinjaController.java
+++ src/main/java/com/AulaSpring/CadastroDeNinjas/ninjas/NinjaController.java
@@ -1,8 +1,6 @@
 package com.AulaSpring.CadastroDeNinjas.ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping //Definir rota
@@ -11,5 +9,30 @@ public class NinjaController {
     @GetMapping("/boasvindas") //Retorna algo por essa rota
     public  String boasVindas(){
         return "1° msg";
+    }
+
+    //FAMOSO CRUDzão da massa!!
+    //Add ninja (CREATE)
+    @PostMapping("/criar")
+    public String criarNinja(){
+        return "ok";
+    }
+    //Mostrar ninjas (READ)
+    @GetMapping("/todos")
+    public String mostrarNinja(){
+        return "ok";
+    }
+
+    //Mostrar ninja por id
+
+    //Alterar dados dos ninjas (UPDATE)
+    @PutMapping("/atualizar")
+    public String atualizarNinja(){
+        return "ok";
+    }
+    //Del ninja (DELETE)
+    @DeleteMapping("/deletar")
+    public String deletarNinja(){
+        return "ok";
     }
 }
